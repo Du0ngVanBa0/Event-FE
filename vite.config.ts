e91b8850fb9d20479d0872cddd2f902d072c30a4
Dương Văn Bảo <tidumong789@gmail.com>
2025-06-05 16:57:15 +0700

    fix build vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Event-FE/',
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  },
+  // Skip ESLint checking during build
+  esbuild: {
+    logOverride: { 'this-is-undefined-in-esm': 'silent' }
+  }
 })
